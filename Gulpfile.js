4d736852ed2f65678a34039a080c68eb880f0ee8
Brad Gignac <bgignac@bradgignac.com>
2014-10-29 16:33:52 -0400

    feat(headers): use Source Sans Pro for headers

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,11 @@
+var async = require('async');
 var gulp = require('gulp');
 
 gulp.task('default', ['build', 'documentation']);
 
-gulp.task('build', function () {
+gulp.task('build', ['build:sass', 'build:fonts'])
+
+gulp.task('build:sass', function () {
   var autoprefixer = require('gulp-autoprefixer');
   var sass = require('gulp-sass');
   var sourcemaps = require('gulp-sourcemaps');
@@ -12,12 +15,16 @@ gulp.task('build', function () {
       .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
       .pipe(sass({ includePaths: ['node_modules/bootstrap-sass/assets/stylesheets'] }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('build:fonts', function () {
+  return gulp.src('fonts/**/*')
+    .pipe(gulp.dest('build/fonts'));
 });
 
 gulp.task('documentation', ['build'], function (done) {
   var join = require('path').join;
-  var async = require('async');
   var metalsmith = require('metalsmith');
   var markdown = require('metalsmith-markdown');
   var templates = require('metalsmith-templates');
